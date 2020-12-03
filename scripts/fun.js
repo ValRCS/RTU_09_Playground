3d06983730f73927a6866b6dc324add48229221c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-03 18:00:58 +0200

    event handling examples

--- scripts/fun.js
+++ scripts/fun.js
@@ -14,8 +14,8 @@ function greetingFun() {
 //calling function
 greetingFun();
 
-//function with a parameter
-function greetName(name) {  
+//function with a default parameter
+function greetName(name = "Valdis") {  
     console.log(`Why hello there ${name}!`);
     console.log("Why hello there " + name + "!");
 }
@@ -42,3 +42,36 @@ console.log("My result is", result);
 //if functions return values we can nest them
 result = mult(mult(5,10), mult(2,3));
 console.log("New result!", result);
+
+//so Valdis is now default for name, when no parameter is given
+greetName();
+greetName("Ilze");
+result = mult(5,10, true);
+//for boolean arguments it is more readable to be
+// explicit on what argument you are using
+mult(10,20, isLogging = true);
+
+const myBtn = document.getElementById("click-btn");
+const myFooter = document.getElementById("my-foot");
+let globalCount = 0;
+
+//i've add a mouse click handler to my button with id click-btn
+//we 
+myBtn.onclick = function () {
+    globalCount++;
+    console.log("I was clicked!", globalCount, "times");
+    myFooter.textContent = "I was clicked!" + globalCount;
+    //I can do more stuff here
+};
+
+//second way of adding a listener
+//we can select elements using our CSS selector syntax
+const colorBtn = document.querySelector("#color-btn");
+
+colorBtn.addEventListener('click', showRandomBackground);
+//main difference between onclick and addEventListener
+//we can add multiple addEventListener handles to same action
+
+
+
+
--- scripts/util.js
+++ scripts/util.js
@@ -4,14 +4,27 @@ function add(a,b) {
     console.log(`${a} + ${b} = ${c}`);
     b = 500; //you can mutate the arguments inside the function
     console.log(b);
+    //this returns undefined
 }
 //a,b,c will be gone once add is finished
 
-function mult(a,b) {
+function mult(a,b, isLogging = false) {
     const c = a * b; //c is a local value(const in this case) 
     //c only lives within this function
-    console.log(`${a} X ${b} = ${c}`);
+    if (isLogging) {
+        console.log(`${a} X ${b} = ${c}`);
+    }
     //we can return the value to be used by other function
     return c; //this means we can assign this value outside the function
     //without explicit return Javascript functions return undefined which is not very useful
 }
+
+function myRandom(number) {
+    return Math.floor(Math.random() * (number+1));
+  }
+
+function showRandomBackground() {
+    const rndCol = 'rgb(' + myRandom(255) + ',' + myRandom(255) + ',' + myRandom(255) + ')';
+    console.log("new color will be", rndCol);
+    document.body.style.backgroundColor = rndCol;
+  }
