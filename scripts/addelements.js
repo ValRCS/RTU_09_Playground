d38f13c9013218ae63803e511f4934da41e15548
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-17 17:20:32 +0200

    Add new elements on click

--- scripts/addelements.js
+++ scripts/addelements.js
@@ -1,12 +1,24 @@
 //our globals will work since we are using defer
 const numA = document.getElementById("num-a");
+const numB = document.getElementById("num-b");
 const addBtn = document.getElementById("add-el");
 const addManyBtn = document.getElementById("add-many");
 const delBtn = document.getElementById("delete-all");
 const elContainer = document.getElementById("app-cont");
 
+function addNewElement(tag, parent, text, id, defClass = "new-el") {
+    const newEl = document.createElement(tag); 
+    newEl.id = id;
+    newEl.classList.add(defClass);
+    newEl.innerText = text;
+    parent.appendChild(newEl);
+}
+
 function onAddBtnClick() {
     console.log("Add button was clicked!");
+    const num = +numA.value;
+    addNewElement("div", elContainer, "Lorem Ipsum "+num, "el-id"+num);
+    //this lets us have multiple elements with same id, thats a minus
 }
 
 function addListeners() {
