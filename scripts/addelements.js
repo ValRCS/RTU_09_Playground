09164153d02c922535b37aa5f12786ef4730580a
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-17 17:43:53 +0200

    addMultipleElements

--- scripts/addelements.js
+++ scripts/addelements.js
@@ -6,6 +6,8 @@ const addManyBtn = document.getElementById("add-many");
 const delBtn = document.getElementById("delete-all");
 const elContainer = document.getElementById("app-cont");
 
+let globalCount = 0; //use globals sparingly
+
 function addNewElement(tag, parent, text, id, defClass = "new-el") {
     const newEl = document.createElement(tag); 
     newEl.id = id;
@@ -14,17 +16,64 @@ function addNewElement(tag, parent, text, id, defClass = "new-el") {
     parent.appendChild(newEl);
 }
 
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        //The list is LIVE so it will re-index each call
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function onDelBtnClick() {
+    console.log("Delete Button was clicked!");
+    removeAllChildren(elContainer);
+    globalCount = 0; //we reset the global count as well
+}
+
 function onAddBtnClick() {
     console.log("Add button was clicked!");
-    const num = +numA.value;
-    addNewElement("div", elContainer, "Lorem Ipsum "+num, "el-id"+num);
+    globalCount++;
+    let className = "";
+    if (globalCount % 2 == 0) {
+        className = "even-class";
+    } else {
+        className = "odd-class";
+    }
+    addNewElement("div", elContainer, "Lorem Ipsum "+globalCount, "el-id"+globalCount, className);
     //this lets us have multiple elements with same id, thats a minus
+}
+
+function onAddManyBtnClick() {
+    const newElCount = parseInt(numB.value); //we need to parse an integer here
+    console.log("Adding", newElCount, " elements at once");
+
+    //simple loop example of adding multiple elements
+    // for (let i=0;i < newElCount; i++) {
+    //     if (i % 2 == 0) {
+    //         className = "even-class";
+    //     } else {
+    //         className = "odd-class";
+    //     }
+    //     addNewElement("div", elContainer, "Lorem Ipsum "+i, "el-id"+i, className);
+    // }
+
+    //here we also update the global element count
+    for (let i=globalCount;i < (globalCount + newElCount); i++) {
+        if (i % 2 == 0) {
+            className = "even-class";
+        } else {
+            className = "odd-class";
+        }
+        addNewElement("div", elContainer, "Lorem Ipsum "+i, "el-id"+i, className);
+    }
+    globalCount = globalCount + newElCount;
 }
 
 function addListeners() {
     console.log("Adding Listeners");
     addBtn.onclick = onAddBtnClick; 
     //alternative is addBtn.addEventListener("click", onAddBtnClick);
+    delBtn.onclick = onDelBtnClick;
+    addManyBtn.onclick = onAddManyBtnClick;
 
 }
 
